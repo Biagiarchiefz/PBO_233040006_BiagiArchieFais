c0fe58bb7d08a794ec47b378fb3c128cb452fcb3
Biagiarchiefz <biagiiarchie@gmail.com>
2025-05-18 09:41:39 +0700

    mengerjakan study kasus yang berbeda dengan yang dimodul

--- src/Abstrack_Interface/StudyKasus/Dimainkan.java
+++ src/Abstrack_Interface/StudyKasus/Dimainkan.java
@@ -1,5 +1,7 @@
 package Abstrack_Interface.StudyKasus;
 
 public interface Dimainkan {
+
+    // membuat kelas interface yang wajib diimpelemtasikan ketika ada class yang mengimplemen class ini
     void play();
 }
